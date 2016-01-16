7de8e976e70b3363bc862ee33ef78e6438587fd8
zealfire <iamalok185@gmail.com>
2016-01-16 12:39:25 +0530

    Now files are being created, some code in comments.

--- udpclient.py
+++ udpclient.py
@@ -4,11 +4,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 10000)
 message = 'This is the message. It will be repeated.'
 try:
-	print >>sys.stderr, 'sending "%s"' % message
+	#print >>sys.stderr, 'sending "%s"' % message
+	message = raw_input()
 	sent = sock.sendto(message, server_address)
-	print >>sys.stderr, 'waiting to receive'
-	data, server = sock.recvfrom(4096)
-	print >>sys.stderr, 'received "%s"' % data
+	#print >>sys.stderr, 'waiting to receive'
+	#data, server = sock.recvfrom(4096)
+	#print >>sys.stderr, 'received "%s"' % data
 
 finally:
 	print >>sys.stderr, 'closing socket'
--- udpserver.py
+++ udpserver.py
@@ -5,11 +5,30 @@ server_address = ('localhost', 10000)
 print >>sys.stderr, 'starting up on %s port %s' %server_address
 sock.bind(server_address)
 while True:
-	print >>sys.stderr, '\nwaiting to receive message'
-	data, address = sock.recvfrom(4096)
-	print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-	print >>sys.stderr, data
-	if data:
-		sent = sock.sendto(data, address)
-    	print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+	print >>sys.stderr, '\nwaiting to receive input'
+	input, address = sock.recvfrom(4096)
+	lists = input.split(',')
+	first = lists[0]
+	second  = lists[1]
+	first_value = first.split(' ')[1]
+	second_value = second.split(' ')[1]
+	if first_value == '"valid"':
+		fp = open("valid.log", "a")
+		sent = second_value + '\n'
+		fp.write(sent)
+		fp.close()
+		#print second_value
+	else:
+		#print "invalid" + second_value
+		fp = open("invalid.log", "a")
+		sent = second_value + "\n"
+		fp.write(sent)
+		fp.close()
+	#print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+	#print >>sys.stderr, data
+	#if data:
+	#	sent = sock.sendto(data, address)
+    #	print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)'''
+    
+    
 
